d0b100d91e224eba179f0456e70920749e494b8a
Marcus Boerger <marcus.borger@snyk.io>
2024-09-01 14:50:23 +0200

    Updates (#10)
    * Updates:
    * Add pre-commit icon to README.
    * Add pre-commit to CI.
    * Improve type handlign in datetime parsing.
    * Add more parsing tests.
    
    * order...

--- mbo/app/flags_test.py
+++ mbo/app/flags_test.py
@@ -27,7 +27,9 @@ from parameterized import param, parameterized
 
 import mbo.app.flags
 
-_NOW = "2024-08-28T14:15:16.123Z"
+_NOW_DATETIME = datetime(2024, 8, 28, 14, 15, 16, 789000, timezone.utc)
+
+_REF_DATETIME = datetime(2023, 7, 28, 20, 21, 23, 456000, timezone.utc)
 
 
 def ActionArgs(name: str = "flag", **kwargs) -> dict[str, Any]:
@@ -59,6 +61,7 @@ class FlagsTest(unittest.TestCase):
     @dataclass_as_param
     @dataclass(kw_only=True)
     class ParseDateTimeOrTimeDeltaTest:
+        test: str
         expected: str
         expected_error: type | None = None
         input: str
@@ -67,62 +70,73 @@ class FlagsTest(unittest.TestCase):
         reference: str | None = None
         error_prefix: str | None = None
         error_suffix: str | None = None
-        now: str = _NOW
+        now: datetime = _NOW_DATETIME
 
     @parameterized.expand(
         [
             ParseDateTimeOrTimeDeltaTest(
+                test="Parse and expand.",
                 expected="2024-04-02T14:00:00Z",
                 input="2024-04-02T14",
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Parse already expanded.",
                 expected="2024-04-02T01:02:03.004Z",
                 input="2024-04-02T01:02:03.004Z",
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Apply midnight.",
                 expected="2024-04-02T00:00:00Z",
                 input="2024-04-02T13:14:15.123Z",
                 midnight=True,
             ),
             ParseDateTimeOrTimeDeltaTest(
-                expected="2024-08-21T14:15:16.123Z",
+                test="Negative time diff, 1 week.",
+                expected="2024-08-21T14:15:16.789Z",
                 input="-1w",
             ),
             ParseDateTimeOrTimeDeltaTest(
-                expected="2024-08-29T14:15:16.123Z",
+                test="Positive time diff, 1 week.",
+                expected="2024-08-29T14:15:16.789Z",
                 input="+1d",
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Positive time diff, 1 day.",
                 expected="2024-08-29T00:00:00Z",
                 input="+1d",
                 midnight=True,
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Positive time diff 8 hours.",
                 expected="2024-08-14 01:00:00Z",
                 input="+8h",
                 reference="2024-08-13 17",
                 midnight=False,
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Positive time diff from reference.",
                 expected="2024-08-14 00:00:00Z",
                 input="+8h",
                 reference="2024-08-13 17",
                 midnight=True,
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Zero time diff",
                 expected="2024-08-14 00:00:00Z",
                 input="+0s",
                 reference="2024-08-14 17",
                 midnight=True,
             ),
             ParseDateTimeOrTimeDeltaTest(
-                expected="Bad timedelta value '+0 NOPE'.",
+                test="Bad time diff",
+                expected="Bad `timedelta` value, must be `int` seconds, not '+0 NOPE'.",
                 expected_error=ValueError,
                 input="+0 NOPE",
                 reference="2024-08-14 17",
                 midnight=True,
             ),
             ParseDateTimeOrTimeDeltaTest(
+                test="Error prefix and suffix.",
                 expected="<prefix>+0 NOPE<suffix>",
                 expected_error=ValueError,
                 input="+0 NOPE",
@@ -134,7 +148,8 @@ class FlagsTest(unittest.TestCase):
         ]
     )
     def test_ParseDateTimeOrTimeDelta(self, test: ParseDateTimeOrTimeDeltaTest):
-        with freeze_time(datetime.fromisoformat(test.now)):
+        self._testMethodDoc = "TEST: " + test.test
+        with freeze_time(_NOW_DATETIME):
             try:
                 self.assertEqual(
                     (
@@ -173,27 +188,32 @@ class FlagsTest(unittest.TestCase):
         expected_error: type | None = None
         action: dict[str, Any]
         input: list[str]
+        now: datetime = _NOW_DATETIME
 
     def FlagTest(self, test: FlagTestData) -> None:
-        try:
-            parser = argparse.ArgumentParser(exit_on_error=False)
-            name = test.action.pop("name", "flag")
-            parser.add_argument(name, **test.action)
-            args = parser.parse_args(test.input)
-            self.assertEqual(
-                test.expected, args.flag, "Bad value in test: " + test.test
-            )
-        except argparse.ArgumentError as error:
-            self.assertIsNotNone(test.expected_error, error)
-            if test.expected_error:
+        self._testMethodDoc = "TEST: " + test.test
+        with freeze_time(_NOW_DATETIME):
+            try:
+                parser = argparse.ArgumentParser(exit_on_error=False)
+                name = test.action.pop("name", "flag")
+                parser.add_argument(name, **test.action)
+                args = parser.parse_args(test.input)
                 self.assertEqual(
-                    test.expected, str(error), "Bad error message in test: " + test.test
+                    test.expected, args.flag, "Bad value in test: " + test.test
                 )
-                self.assertEqual(
-                    type(error),
-                    test.expected_error,
-                    "Bad error type in test: " + test.test,
-                )
+            except argparse.ArgumentError as error:
+                self.assertIsNotNone(test.expected_error, error)
+                if test.expected_error:
+                    self.assertEqual(
+                        test.expected,
+                        str(error),
+                        "Bad error message in test: " + test.test,
+                    )
+                    self.assertEqual(
+                        type(error),
+                        test.expected_error,
+                        "Bad error type in test: " + test.test,
+                    )
 
     @parameterized.expand(
         [
@@ -336,6 +356,15 @@ class FlagsTest(unittest.TestCase):
                 input=["2024-01-30T13:14:51"],
             ),
             FlagTestData(
+                test="Verify only: Parse from iso datetime.",
+                expected="2024-01-30T13:14:51",
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    verify_only=True,
+                ),
+                input=["2024-01-30T13:14:51"],
+            ),
+            FlagTestData(
                 test="Parse from iso datetime applying midnight.",
                 expected=datetime(year=2024, month=1, day=30, tzinfo=timezone.utc),
                 action=ActionArgs(
@@ -353,6 +382,77 @@ class FlagsTest(unittest.TestCase):
                 input=["20240130"],
             ),
             FlagTestData(
+                test="Verify only: Parse from short datetime.",
+                expected="20240130",  # Midnight not applied.
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    verify_only=True,
+                ),
+                input=["20240130"],
+            ),
+            FlagTestData(
+                test="Parse default from short datetime.",
+                expected=datetime(year=2024, month=2, day=3, tzinfo=timezone.utc),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    default="20240203",
+                    nargs="?",
+                ),
+                input=[],
+            ),
+            FlagTestData(
+                test="Use default from datetime.",
+                expected=datetime(year=2024, month=2, day=4, tzinfo=timezone.utc),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    default=datetime(2024, 2, 4),
+                    nargs="?",
+                ),
+                input=[],
+            ),
+            FlagTestData(
+                test="Use bad type for default.",
+                expected="argument flag: Default value must be None or of type `datetime` or `str`, provided is `<class 'bool'>`.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    default=False,
+                    nargs="?",
+                ),
+                input=[],
+            ),
+            FlagTestData(
+                test="Verify only: Use bad type for default.",
+                expected="argument flag: Default value must be None or of type `datetime` or `str`, provided is `<class 'bool'>`.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    default=False,
+                    nargs="?",
+                    verify_only=True,
+                ),
+                input=[],
+            ),
+            FlagTestData(
+                test="Use default for default which is now.",
+                expected=_NOW_DATETIME,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    nargs="?",
+                ),
+                input=[],
+            ),
+            FlagTestData(
+                test="Verify only: Use default for default which is now.",
+                expected=str(_NOW_DATETIME),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    nargs="?",
+                    verify_only=True,
+                ),
+                input=[],
+            ),
+            FlagTestData(
                 test="Parse from short datetime, bad input.",
                 expected="argument flag: Invalid date string: '20240230', day is out of range for month",
                 expected_error=argparse.ArgumentError,
@@ -362,33 +462,113 @@ class FlagsTest(unittest.TestCase):
                 input=["20240230"],
             ),
             FlagTestData(
-                test="Parse from timedelta.",
-                expected=datetime(2024, 9, 4, 14, 15, 16, 123000, timezone.utc),
+                test="Verify only: Parse from short datetime, bad input.",
+                expected="argument flag: Invalid date string: '20240230', day is out of range for month",
+                expected_error=argparse.ArgumentError,
                 action=ActionArgs(
                     action=mbo.app.flags.ActionDateTimeOrTimeDelta,
-                    reference=_NOW,
+                    verify_only=True,
+                ),
+                input=["20240230"],
+            ),
+            FlagTestData(
+                test="Parse from time diff.",
+                expected=datetime(2023, 8, 4, 20, 21, 23, 456000, timezone.utc),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=_REF_DATETIME,
                 ),
                 input=["+1w"],
             ),
             FlagTestData(
-                test="Parse from timedelta applying midnight.",
-                expected=datetime(2024, 9, 4, 0, 0, 0, 0, timezone.utc),
+                test="Parse from time diff applying midnight.",
+                expected=datetime(2023, 8, 4, 0, 0, 0, 0, timezone.utc),
                 action=ActionArgs(
                     action=mbo.app.flags.ActionDateTimeOrTimeDelta,
-                    reference=_NOW,
+                    reference=_REF_DATETIME,
                     midnight=True,
                 ),
                 input=["+1w"],
             ),
             FlagTestData(
-                test="Parse from negative timedelta.",
-                expected=datetime(2024, 8, 21, 14, 15, 16, 123000, timezone.utc),
+                test="Verify only: Parse from time diff.",
+                expected="+1w",
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=_REF_DATETIME,
+                    verify_only=True,
+                ),
+                input=["+1w"],
+            ),
+            FlagTestData(
+                test="Parse flag from negative time diff.",
+                expected=datetime(2023, 7, 21, 20, 21, 23, 456000, timezone.utc),
                 action=ActionArgs(
                     "--flag",
                     action=mbo.app.flags.ActionDateTimeOrTimeDelta,
-                    reference=_NOW,
+                    reference=_REF_DATETIME,
                 ),
                 input=["--flag=-1w"],
+            ),
+            FlagTestData(
+                test="Parse arg from negative time diff to reference datetime.",
+                expected=datetime(2023, 7, 21, 20, 21, 23, 456000, timezone.utc),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=_REF_DATETIME,
+                ),
+                input=["--", "-1w"],
+            ),
+            FlagTestData(
+                test="Parse arg from negative time diff to reference str.",
+                expected=datetime(2023, 7, 21, 20, 21, 23, 456000, timezone.utc),
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=str(_REF_DATETIME),
+                ),
+                input=["--", "-1w"],
+            ),
+            FlagTestData(
+                test="Parse from bad time diff value.",
+                expected="argument flag: Bad `timedelta` value, must be `int` seconds, not '+1'.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=_REF_DATETIME,
+                ),
+                input=["+1"],
+            ),
+            FlagTestData(
+                test="Verify only: Parse from bad time diff value.",
+                expected="argument flag: Bad `timedelta` value, must be `int` seconds, not '+1'.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=_REF_DATETIME,
+                    verify_only=True,
+                ),
+                input=["+1"],
+            ),
+            FlagTestData(
+                test="Parse from bad time diff type.",
+                expected="argument flag: Reference value must be None or of type `datetime` or `str`, provided is `<class 'bool'>`.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=False,
+                ),
+                input=["+1"],
+            ),
+            FlagTestData(
+                test="Verify only: Parse from bad time diff type.",
+                expected="argument flag: Reference value must be None or of type `datetime` or `str`, provided is `<class 'bool'>`.",
+                expected_error=argparse.ArgumentError,
+                action=ActionArgs(
+                    action=mbo.app.flags.ActionDateTimeOrTimeDelta,
+                    reference=False,
+                    verify_only=True,
+                ),
+                input=["+1"],
             ),
         ]
     )
